5d048d761f91c8176bad69734cb76b95eb9878ec
Roman Gnatyuk <54001169+mort-gh@users.noreply.github.com>
2020-08-13 09:09:13 +0300

    Connect to peerjs

--- public/script.js
+++ public/script.js
@@ -3,6 +3,13 @@ const socket = io('/');
 const videoGrid = document.getElementById('video-grid');
 const myVideo = document.createElement('video');
 myVideo.muted = true;
+
+var peer = new Peer(undefined, {
+  path: '/peerjs',
+  host: '/',
+  port: '3030',
+});
+
 let myVideoStream;
 
 navigator.mediaDevices
@@ -13,9 +20,31 @@ navigator.mediaDevices
   .then((stream) => {
     myVideoStream = stream;
     addVideoStream(myVideo, stream);
+
+    peer.on('call', (call) => {
+      call.answer(stream);
+      const video = document.createElement('video');
+      call.on('stream', (userVideoStream) => {
+        addVideoStream(video, userVideoStream);
+      });
+    });
+
+    socket.on('user-connected', (userId) => {
+      connectToNewUser(userId, stream);
+    });
   });
 
-socket.emit('join-room', ROOM_ID);
+peer.on('open', (id) => {
+  socket.emit('join-room', ROOM_ID, id);
+});
+
+const connectToNewUser = (userId, stream) => {
+  const call = peer.call(userId, stream);
+  const video = document.createElement('video');
+  call.on('stream', (userVideoStream) => {
+    addVideoStream(video, userVideoStream);
+  });
+};
 
 const addVideoStream = (video, stream) => {
   video.srcObject = stream;
--- server.js
+++ server.js
@@ -2,10 +2,13 @@ const express = require('express');
 const app = express();
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
+const { ExpressPeerServer } = require('peer');
+const peerServer = ExpressPeerServer(server, { debug: true });
 const { v4: uuidv4 } = require('uuid');
 
 app.set('view engine', 'ejs');
 
+app.use('/peerjs', peerServer);
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
@@ -17,7 +20,7 @@ app.get('/:room', (req, res) => {
 });
 
 io.on('connection', (socket) => {
-  socket.on('join-room', () => {
+  socket.on('join-room', (roomId, userId) => {
     socket.join(roomId);
     socket.to(roomId).broadcast.emit('user-connected', userId);
   });
